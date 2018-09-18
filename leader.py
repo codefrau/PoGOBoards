afe7b969d69cd1b08bb0e00f5dc4960decb156d1
Bert Freudenberg <bert@freudenbergs.de>
2018-09-17 19:25:11 -0700

    Fancier formatting with discord handles

--- leader.py
+++ leader.py
@@ -8,13 +8,13 @@ reData = re.compile('^([0-9:T-]+) +([0-9,]+) +([0-9,]+) +([0-9,]+) +([0-9,]+) +(
 trainers = {}
 
 for line in open('data.txt', 'r'):
-    match = reData.match(line)
+    match = reData.match(line.decode("utf-8"))
     date   = datetime.datetime.strptime(match.group(1), "%Y-%m-%dT%H:%M:%S")
     catch  = int(match.group(2))
     walk   = int(match.group(3))
     battle = int(match.group(4))
     xp     = int(match.group(5))
-    name   = match.group(6).decode("utf-8")
+    name   = match.group(6)
 
     #print date, catch, walk, battle, xp, name
     
@@ -32,12 +32,19 @@ for line in open('data.txt', 'r'):
         "stats": [catch, walk, battle, xp]
     })
 
-SURROGATE_PAIR = re.compile(u'[\ud800-\udbff][\udc00-\udfff]', re.UNICODE)
-def unicodeLen(s):
-    return len(SURROGATE_PAIR.sub('.', s))
+reName = re.compile('^((.*)#[0-9]+) ?(.*)$')
+
+for line in open('names.txt', 'r'):
+    match = reName.match(line.decode("utf-8"))
+    handle = match.group(1)
+    name = (match.group(3) or match.group(2))
+    if name in trainers:
+        trainers[name]["handle"] = handle
+    else:
+        print line
+        raise Exception("Name not in data: '%s'" % name)
 
 board = []
-longest_name = 0
 
 for name in trainers:
     entries = trainers[name]["entries"]
@@ -55,24 +62,31 @@ for name in trainers:
         "name":   name,
         "scores":  per_week,
     })
-    length = unicodeLen(name)
-    if length > longest_name:
-        longest_name = length
 
 titles = [":badge_catch: Number of Pokemon caught", ":badge_walk: KM walked", ":badge_battle: Battles fought", ":badge_xp: XP gained"]
+places = [":one:", ":two:", ":three:", ":four:", ":five:", ":six:", ":seven:", ":eight:", ":nine:", ":keycap_ten:"]
+
+FANCY = True
 
 for category, title in enumerate(titles):
     print "**%s per week:**" % title
-    print "```"
+    if not FANCY:
+        print "```"
     board.sort(key=lambda trainer: trainer["scores"][category], reverse = True)
     formatted_numbers = ["{:,}".format(trainer["scores"][category]) for trainer in board]
     longest_number = max([len(str(n)) for n in formatted_numbers])
     for i, trainer in enumerate(board):
         name = trainer["name"]
-        name = name + " " * (longest_name - unicodeLen(name))
         score = formatted_numbers[i]
-        score = " " * (longest_number - len(score)) + score
-        print "%2d   %s   %s" % (i + 1, score, name)
+        if FANCY:
+            try:
+                handle = trainers[name]["handle"]
+            except:
+                raise Exception("%s not found in names.txt" % name.encode('utf-8'))
+            print "%s **%s** @%s" % (places[i], score, handle)
+        else:
+            score = " " * (longest_number - len(score)) + score
+            print "%2d. %s %s" % (i+1, score, name)
         if i == 9:
             break
-    print "```"
+    print "" if FANCY else "```"
