ca167729a38f209bdab97382d31c22e1069e3411
Bert Freudenberg <bert@freudenbergs.de>
2018-11-01 11:34:48 -0700

    past-raw.sh: delete older raw files, do not call leader script

--- paste-raw.sh
+++ paste-raw.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+[ "$USER" == "bert" ] && rm -f `git ls-files --others --exclude-standard | grep ^raw/`
 file=raw/`date +'%Y-%m-%dT%H:%M:%S%z'`.txt
 echo "Pasting to $file"
 pbpaste > "$file"
-./raw-to-data.py > data.txt && ./leader.py > boards.txt && less boards.txt
+./raw-to-data.py > data.txt && git diff data.txt
