69001da236a9033ce547290ae682760494c0aa05
Bert Freudenberg <bert@freudenbergs.de>
2020-04-07 10:11:11 -0700

    change user name

--- paste-raw.sh
+++ paste-raw.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-[ "$USER" == "bert" ] && rm -f `git ls-files --others --exclude-standard | grep ^raw/`
+[ "$USER" == "vanessa" ] && rm -f `git ls-files --others --exclude-standard | grep ^raw/`
 file=raw/`date +'%Y-%m-%dT%H:%M:%S%z'`.txt
 echo "Pasting to $file"
 pbpaste > "$file"
